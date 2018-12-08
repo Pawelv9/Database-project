cdccbaadd115e501b8260831bb1b13e70918047c
Pavel <Pawelv9@gmail.com>
2018-12-08 15:20:04 +0300

    Initial commit

---
+++ test/test_helper.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// default Promise implementation
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', () => {
+            console.warn('Warning', error);
+        });
+});
+
+beforeEach(done => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next tests
+    done();
+  });
+});
